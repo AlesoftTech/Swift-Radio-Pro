fa8ea078b4824fd60966b02a7fc4f8c2ec7d44a7
macOS Sierra <da.solano1@uniandes.edu.co>
2017-07-17 21:43:01 -0500

    s

---
+++ SwiftRadio/PagVIew.swift
@@ -0,0 +1,18 @@
+//
+//  PagView.swift
+//  SwiftRadio
+//
+//  Created by macOS Sierra on 17/07/17.
+//  Copyright © 2017 matthewfecher.com. All rights reserved.
+//
+
+import UIKit
+class PagView : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning();
+        
+    }
+}
---
+++ SwiftRadio/PagViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PagViewController.swift
+//  SwiftRadio
+//
+//  Created by macOS Sierra on 17/07/17.
+//  Copyright © 2017 matthewfecher.com. All rights reserved.
+//
+
+import UIKit
+class UIPageViewController: UIPageViewController{
+    override func viewDidLoad(){
+        super.viewDidLoad(); 
+    }
+}
