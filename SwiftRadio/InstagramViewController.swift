b07039929fbfd215d0d6694d287268fd406ce460
macOS Sierra <da.solano1@uniandes.edu.co>
2017-07-18 15:17:14 -0500

    ecs di

---
+++ SwiftRadio/InstagramViewController.swift
@@ -0,0 +1,39 @@
+//
+//  InstagramViewController.swift
+//  SwiftRadio
+//
+//  Created by macOS Sierra on 18/07/17.
+//  Copyright © 2017 matthewfecher.com. All rights reserved.
+//
+
+import UIKit
+
+class InstagramViewController: UIViewController {
+
+    
+    @IBOutlet weak var myWebView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: "https://www.instagram.com/notidelllano/")
+        myWebView.loadRequest(URLRequest(url:url!))
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
